5258a99044ff66ec58cbd2138d8078422c386ab7
kencanak <kesuma.kencana@yahoo.com>
2016-02-29 02:44:32 +0800

    wrapping up game history listing

---
+++ client/components/game-history/game-history.controller.js
@@ -0,0 +1,53 @@
+'use strict';
+
+angular.module('foosballApp')
+  .controller('gameHistoryCtrl', function ($scope, $location, Auth, Game, showGameForm, notify, gamePlayers, $moment) {
+    $scope.players = angular.copy(gamePlayers);
+
+    $scope.showGameForm = showGameForm;
+
+    $scope.isCollapsed = true;
+    $scope.isLoggedIn = Auth.isLoggedIn;
+    $scope.isAdmin = Auth.isAdmin;
+    $scope.getCurrentUser = Auth.getCurrentUser;
+    $scope.doneCalc = false;
+    $scope.gameHistory = {}
+
+
+    sortGames();
+
+    $scope.$watch('showGameForm.added', function(newValue, oldValue){
+      if (newValue === true) {
+        sortGames();
+      }
+    });
+
+    $scope.getLength = function(obj) {
+      return Object.keys(obj).length;
+    }
+
+
+
+    function sortGames(){
+      $scope.gameHistory = {}
+      Game.history().$promise.then(function(data) {
+        angular.forEach(data, function(value, key){
+
+          if (!(moment(value.created).format('MMMM YYYY') in $scope.gameHistory)) {
+            $scope.gameHistory[moment(value.created).format('MMMM YYYY')] = {}
+
+          }
+
+          if (!(moment(value.created).format('dddd DD') in $scope.gameHistory[moment(value.created).format('MMMM YYYY')])) {
+            $scope.gameHistory[moment(value.created).format('MMMM YYYY')][moment(value.created).format('dddd DD')] = []
+          }
+
+          $scope.gameHistory[moment(value.created).format('MMMM YYYY')][moment(value.created).format('dddd DD')].push(value);
+        });
+        $scope.doneCalc = true;
+        console.log($scope.gameHistory);
+      });
+
+    }
+
+  });
